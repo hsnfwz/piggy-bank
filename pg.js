63b1975c6d5345619f6d16e6b10940ee979149d8
Hussein Fawaz <husseinfawaz@MacBook-Pro-7.local>
2024-05-15 17:34:02 -0700

    name change

--- pg.js
+++ pg.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 
-// const connectionString = 'postgres://postgres:postgres@localhost:5432/saveinn';
 const connectionString = `postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOSTNAME}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const pool = new Pool({ connectionString });
 
